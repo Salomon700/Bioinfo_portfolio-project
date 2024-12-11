4f546ca9f225a470549d39ac6cadd60bd830208f
Salomon700 <ntwalisolomon9@gmail.com>
2024-12-11 19:37:01 +0200

    Front-end complete

--- website/routes.py
+++ website/routes.py
@@ -5,3 +5,15 @@ routes = Blueprint('routes', __name__)
 @routes.route('/')
 def home():
     return render_template("home.html")
+
+@routes.route('/pairwise-alignment')
+def pairwise_alignment():
+    return render_template('pairwise_alignment.html')
+
+@routes.route('/multiple-alignment')
+def multiple_alignment():
+    return render_template('multiple_alignment.html')
+
+@routes.route('/phylogenetic-tree')
+def phylogenetic_tree():
+    return render_template('phylogenetic_tree.html')
